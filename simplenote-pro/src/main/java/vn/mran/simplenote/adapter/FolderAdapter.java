97c38e7414d569c7f1bcdf50e99db04da534e951
AnPham <an.phamhung@gmail.com>
2016-08-23 13:14:24 +0700

    update button swipe

--- simplenote-pro/src/main/java/vn/mran/simplenote/adapter/FolderAdapter.java
+++ simplenote-pro/src/main/java/vn/mran/simplenote/adapter/FolderAdapter.java
@@ -2,11 +2,15 @@ package vn.mran.simplenote.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.daimajia.swipe.SwipeLayout;
 
 import io.realm.RealmResults;
 import vn.mran.simplenote.R;
@@ -22,11 +26,17 @@ public class FolderAdapter extends RecyclerView.Adapter<FolderAdapter.MyViewHold
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView txtItem;
         public LinearLayout row;
+        SwipeLayout swipeLayout;
+        Button btnDelete;
+        Button btnRename;
 
         public MyViewHolder(View view) {
             super(view);
             txtItem = (TextView) view.findViewById(R.id.txtItem);
             row = (LinearLayout) view.findViewById(R.id.row);
+            swipeLayout = (SwipeLayout) itemView.findViewById(R.id.swipe);
+            btnDelete = (Button) itemView.findViewById(R.id.btnDelete);
+            btnRename = (Button) itemView.findViewById(R.id.btnRename);
         }
     }
 
@@ -48,14 +58,29 @@ public class FolderAdapter extends RecyclerView.Adapter<FolderAdapter.MyViewHold
         final Folder folder = realmResult.get(position);
         TouchEffect.addAlpha(holder.row);
         holder.txtItem.setText(folder.getName());
+        holder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
         setOnClick(holder,folder);
     }
 
-    private void setOnClick(MyViewHolder myViewHolder, final Folder folder) {
+    private void setOnClick(final MyViewHolder myViewHolder, final Folder folder) {
         myViewHolder.row.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialogSelectFolderView.onSelectItem(folder);
+            }
+        });
+        myViewHolder.btnRename.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("asdasd","rename");
+                myViewHolder.swipeLayout.close();
+            }
+        });
+        myViewHolder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("asdasd","delete");
+                myViewHolder.swipeLayout.close();
             }
         });
     }
